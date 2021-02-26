3ffa59a8480849cdd62107f50c6b8f40fd0d5000
mbronstein <mark@bronsteinlaw.com>
2021-02-26 13:47:14 -0500

    changed cateogry model to a subclass of django-categores base model

--- blog/models/category_models.py
+++ blog/models/category_models.py
@@ -2,12 +2,11 @@
 from django.db import models
 from django.urls import reverse
 from django.utils.text import slugify
+from categories.base import CategoryBase
 
 
-class Category(models.Model):
+class Category(CategoryBase):
 
-    name = models.CharField(max_length=100, null=False, blank=False)
-    slug = models.SlugField()
     image = models.ImageField(default='category-default.jpg',
                               upload_to='category_images')
     approved = models.BooleanField(default=True)
@@ -15,16 +14,8 @@ class Category(models.Model):
     date_updated = models.DateTimeField(auto_now=True)
 
     class Meta:
-        unique_together = ('name',)
         verbose_name = 'category'
         verbose_name_plural = 'categories'
-
-    def __str__(self):
-        return self.name
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name, allow_unicode=True)
-        super(Category, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('blog:category_articles',
